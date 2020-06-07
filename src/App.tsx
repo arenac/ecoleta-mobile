7befbf5d82d1439ca405413de6407507362719c9
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-07 16:07:38 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+declare const global: {HermesInternal: null | {}};
+
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
+};
+
+export default App;
